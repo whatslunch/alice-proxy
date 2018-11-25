8e7cd976c8c43756e6b119ca01206236f8c11c95
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-24 23:40:38 -0800

    refactored proxy server and wrote stress test

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 1000;
+const port = 8080;
 
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+app.all('/*', (req, res) => {
+  const url = 'http://localhost:5882' + req.url;
+  let headers = Object.create(req.headers);
+  delete headers['connection'];
+  const options = {
+    method: req.method,
+    headers: headers,
+    params: req.query,
+    data: req.body,
+    url,
+  };
+  axios(options)
+    .then((result) => {
+      res.statusMessage = result.statusText;
+      res.set(result.headers).status(result.status).send(result.data);
+    })
+    .catch((error) => {
+      if (error.response !== undefined) {
+        res.statusMessage = error.response.statusText;
+        res.set(error.response.headers).status(error.response.status).send(error.response.data);
+      } else {
+        res.statusMessage = 'Back-end server is at capacity';
+        res.status(503).send();
+      }
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
